dbcd8dd8950f41dd3b435f4a2e6636162f15845d
Usman Ghani <33672734+usmansahi@users.noreply.github.com>
2022-12-22 21:24:50 +0100

    real time chat app

--- js/client.js
+++ js/client.js
@@ -3,15 +3,37 @@ const socket = io("http://localhost:8000");
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".container");
+var audio = new Audio("t.mp3");
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+  if (position == "left") {
+    audio.play();
+  }
+};
 
-
-const append = (message, position) =>{
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageElement.classList.add('message')
-    messageElement.classList.add('')
-}
-
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`you: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
 
 const name1 = prompt("Enter your name to join");
 socket.emit("new-user-joined", name1);
+
+socket.on("user-joined", (name1) => {
+  append(`${name1} joined the chat`, "right");
+});
+
+socket.on("receive", (data) => {
+  append(`${data.name1}: ${data.message}`, "left");
+});
+
+socket.on("left", (name1) => {
+  append(`${name1}: left the chat`, "left");
+});
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -4,14 +4,19 @@ const users = {};
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (name1) => {
-    console.log("New user", name1);
+    /*  console.log("New user", name1); */
     users[socket.id] = name1;
     socket.broadcast.emit("user-joined", name1);
   });
   socket.on("send", (message) => {
     socket.broadcast.emit("receive", {
       message: message,
-      name1: user[socket.id],
+      name1: users[socket.id],
     });
+  });
+
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
   });
 });
